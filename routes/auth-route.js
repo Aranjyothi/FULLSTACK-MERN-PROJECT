dd84d59e13f5bde72c25218a3bca7077805110a4
jyothipropelo <88416663+Aranjyothi@users.noreply.github.com>
2022-11-16 12:36:14 -0500

    changes

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -6,4 +6,6 @@ const authCtrl = require('../controller/auth-controller')
 router.post('/signup', authCtrl.signup)
 router.post('/login', authCtrl.login)
 
+router.get('/test',authCtrl.testroute)
+
 module.exports = router
